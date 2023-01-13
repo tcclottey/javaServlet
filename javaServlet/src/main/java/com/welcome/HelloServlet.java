1d54821acc13e3357df055dcfdd5de34c449e1d4
LeslieClottey <tcclotteygh@gmail.com>
2023-01-12 23:36:56 -0500

    Initial commit

---
+++ javaServlet/src/main/java/com/welcome/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.welcome;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				response.setContentType("txt.html");
+				PrintWriter out = response.getWriter();
+				out.print("<html><body>");
+				out.print("<h2>Welcome to Leslie's Code Space</h2>");
+				out.print("</body></html>");
+	}
+
+}
